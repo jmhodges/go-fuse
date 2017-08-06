bc9b711f88e00f10059428d8589b10f7c632671c
Jeff Hodges <jeff@somethingsimilar.com>
2017-08-05 18:34:38 -0700

    well

--- all.bash
+++ all.bash
@@ -19,7 +19,6 @@ do
         for GOOS in darwin linux ; do
           export GOOS
           go test -c -i github.com/hanwen/go-fuse/$d
-          go test -c -i -race github.com/hanwen/go-fuse/$d
         done
 
         echo "go test github.com/hanwen/go-fuse/$d"
